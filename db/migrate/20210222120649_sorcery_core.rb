dfaa32fa2aaa0950ce43e789cb6058b129d7df51
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-22 21:07:27 +0900

    rails g sorcery:install external --force.

--- db/migrate/20210222120649_sorcery_core.rb
+++ db/migrate/20210222120649_sorcery_core.rb
@@ -1,6 +1,6 @@
 class SorceryCore < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :User do |t|
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
@@ -8,6 +8,6 @@ class SorceryCore < ActiveRecord::Migration[6.0]
       t.timestamps                null: false
     end
 
-    add_index :users, :email, unique: true
+    add_index :User, :email, unique: true
   end
 end
