6e59bb631e1038d89f0c00b60cd8d6c13e937bf4
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-23 18:04:49 +0900

    implement methods.

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,7 +1,25 @@
 class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+
   def oauth
+    login_at(params[:provider])
   end
 
   def callback
+    provider = auth_params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+    else
+      begin
+        User.find_by(email: @user.email)
+      rescue
+        redirect_to root_path, notice: "Failed to login from #{provider.titleize}!"
+      end
+    end
+  end
+
+  private
+  def auth_params
+    params.permit(:code, :provider, :session_state)
   end
 end
