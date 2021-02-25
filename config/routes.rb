e9f032b408ceb4f77ddd744315c52cf4030e209a
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-25 20:20:21 +0900

    delete oauth root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'oauths/oauth'
-  get 'oauths/callback'
   root to: "welcome#index"
-  resources :users
-
-  post "oauth/callback" => "oauths#callback"
-  get "/oauth/callback/microsoft" => "oauths#callback"
-  get "oauth/callback" => "oauths#callback"
-  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 end
