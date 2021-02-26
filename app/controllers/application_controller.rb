479b8336d9790d867fa7ee1f5a5ffc9afb4db549
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-26 20:59:15 +0900

    create a method that store session info from auth hash data.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def save_in_session(auth_hash)
+    session[:graph_token_hash] = auth_hash[:credentials]
+    session[:user_name] = auth_hash.dig(:extra, :raw_info, :displayName)
+  end
 end
