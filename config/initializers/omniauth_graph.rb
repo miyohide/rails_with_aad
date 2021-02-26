e0bf75c06004845a47347549c0d5a3d04724c03e
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-26 20:55:13 +0900

    fix scope

--- config/initializers/omniauth_graph.rb
+++ config/initializers/omniauth_graph.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :microsoft_graph_auth,
            Rails.application.credentials.azure_app_id,
            Rails.application.credentials.azure_app_secret,
-           :scope => Rails.application.credentials.azure_scopes
+           :scope => 'openid profile offline_access user.read'
 end
