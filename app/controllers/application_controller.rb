d253dcc455e84ee68c021bf7f69f8499a8eb2295
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-26 21:02:16 +0900

    add getter methods (user_name/access_token).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
     session[:graph_token_hash] = auth_hash[:credentials]
     session[:user_name] = auth_hash.dig(:extra, :raw_info, :displayName)
   end
+
+  def user_name
+    session[:user_name]
+  end
+
+  def access_token
+    session[:graph_token_hash][:token]
+  end
 end
