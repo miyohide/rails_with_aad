90a8c8600fb20770cda5e24327a74fa69aad4e83
Hidenori Miyoshi <miyohide@gmail.com>
2021-02-27 18:47:45 +0900

    add set_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
   def save_in_session(auth_hash)
     session[:graph_token_hash] = auth_hash[:credentials]
     session[:user_name] = auth_hash.dig(:extra, :raw_info, :displayName)
@@ -10,5 +12,9 @@ class ApplicationController < ActionController::Base
 
   def access_token
     session[:graph_token_hash][:token]
+  end
+
+  def set_user
+    @user_name = user_name
   end
 end
