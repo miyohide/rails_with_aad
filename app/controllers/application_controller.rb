33e0e08493c269cd1335a6a9c8439d78d5288da4
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-01 20:53:20 +0900

    implement refresh_tokens method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require "microsoft_graph_auth"
+require "oauth2"
+
 class ApplicationController < ActionController::Base
   before_action :set_user
 
@@ -16,5 +19,19 @@ class ApplicationController < ActionController::Base
 
   def set_user
     @user_name = user_name
+  end
+
+  def refresh_tokens(token_hash)
+    oauth_strategy = OmniAuth::Strategies::MicrosoftGraphAuth.new(
+      nil, Rails.application.credentials.azure_app_id,
+      Rails.application.credentials.azure_app_secret
+    )
+    token = OAuth2::AccessToken.new(
+      oauth_strategy.client, token_hash[:token],
+      refresh_token: token_hash[:refresh_token]
+    )
+    new_tokens = token.refresh!.to_hash.slice(:access_token, :refresh_token, :expires_at)
+    new_tokens[:token] = new_tokens.delete :access_token
+    session[:graph_token_hash] = new_tokens
   end
 end
