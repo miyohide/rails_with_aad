61035b84a30adbf86c90ce4f9ab99749fb183ae3
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-04 18:21:33 +0900

    implement index action.

--- app/controllers/user_detail_controller.rb
+++ app/controllers/user_detail_controller.rb
@@ -1,4 +1,15 @@
 class UserDetailController < ApplicationController
+  include GraphHelper
+
   def index
+    @user_detail = make_api_call("GET", "/v1.0/me", access_token)
+    render json: @user_detail
+    # rescue RuntimeError => e
+    #   @errors = [
+    #     {
+    #       message: "Microsoft Graph returned an error getting user info",
+    #       debug: e
+    #     }
+    #   ]
   end
 end
