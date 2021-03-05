5ffbe7787e30631af98b4a4a37cb9704b4f5b441
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-05 21:12:42 +0900

    create key array

--- app/controllers/user_detail_controller.rb
+++ app/controllers/user_detail_controller.rb
@@ -2,6 +2,18 @@ class UserDetailController < ApplicationController
   include GraphHelper
 
   def index
+    @keys = %w(
+      businessPhones
+      displayName
+      givenName
+      jobTitle
+      mail
+      mobilePhone
+      officeLocation
+      preferredLanguage
+      surname
+      userPrincipalName
+    )
     @user_detail = make_api_call("GET", "/v1.0/me", access_token)
     rescue RuntimeError => e
       @errors = [
